b18aac3a4ce6ed0c0ebf155dadbd7d31ea7ad0d3
Ilya Nikolaev <65247719+ilya-nikolaev@users.noreply.github.com>
2025-05-10 14:02:00 +0300

    Added entrypoint

---
+++ src/badge_gen/main/entrypoint.py
@@ -0,0 +1,33 @@
+import logging
+
+from dishka import make_async_container
+from dishka.integrations.fastapi import setup_dishka
+from fastapi import FastAPI
+
+from badge_gen.handlers import leetcode_router
+from badge_gen.main.config import Config, load_config
+from badge_gen.main.di.providers import HTTPProvider, TemplateProvider
+
+
+def create_app() -> FastAPI:
+    logging.basicConfig(level=logging.DEBUG)
+
+    config = load_config()
+
+    container = make_async_container(
+        HTTPProvider(),
+        TemplateProvider(),
+        context={
+            Config: config,
+        },
+    )
+
+    app = FastAPI()
+    setup_dishka(container, app)
+
+    app.include_router(leetcode_router)
+
+    return app
+
+
+app = create_app()
