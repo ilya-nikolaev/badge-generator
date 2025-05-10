1743c1be4a1574a730701fec8f440781d739bdb8
Ilya Nikolaev <65247719+ilya-nikolaev@users.noreply.github.com>
2025-05-10 14:02:00 +0300

    Added config loader

---
+++ src/badge_gen/main/config.py
@@ -0,0 +1,30 @@
+import logging
+import tomllib
+from pathlib import Path
+
+from pydantic import BaseModel
+
+logger = logging.getLogger(__name__)
+
+type Username = str
+
+
+class AccessConfig(BaseModel):
+    white_list: dict[str, list[Username]]
+
+
+class TemplateConfig(BaseModel):
+    path: Path
+
+
+class Config(BaseModel):
+    access: AccessConfig
+    template: TemplateConfig
+
+
+def load_config() -> Config:
+    config_path = Path.cwd() / ".config" / "config.toml"
+    logger.debug("Config path: %s", config_path)
+
+    with config_path.open("rb") as file:
+        return Config(**tomllib.load(file))
